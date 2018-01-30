6b109fc09f90032ed606c38786565f91ce89d1e3
Steven Bruce <Brucesc726@gmail.com>
2018-01-30 12:16:09 -0800

    made some changes to the query

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,5 +1,5 @@
 select c.id, Name as 'Customer Name', Date, Amount 
-from [order] o
-join customer c
-	on o.customerid = c.id
-order by c.name, o.amount desc
+from [order] o -- the order table with alias o
+join customer c -- joining customer table with alias c
+	on o.customerid = c.id -- setting the join to match like columns in o and c tables
+order by c.name, o.amount desc -- the order columns will display the rows in asc/desc
